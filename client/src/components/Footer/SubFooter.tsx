47d67c1703307763385bc5fdbf20f32360358f9c
Aspirin <samuellyworld@gmail.com>
2023-11-12 03:58:02 +0100

    chore : small changes MVC (#10) 🏷️

--- client/src/components/Footer/SubFooter.tsx
+++ client/src/components/Footer/SubFooter.tsx
@@ -1,25 +1,26 @@
-import { SubFooterContainer} from "./SubFooter.styles";
+import { SubFooterContainer } from "./SubFooter.styles";
 import ShareImage from "/assets/png/share.png";
 const SubFooter = () => {
-    return (
-      <SubFooterContainer>
-          <div className="SubFooterDiv">
-          <div className="shareDiv">
-            <p className="share">Share <br /> Crypto via <br /> juat a link<span className="dot" >.</span></p>
-            <div className="perfect">
-              <p>perfect for</p>
-              <p className="gifts">gifts</p>
-              <p className="gifts">payments</p>
-              <p className="gifts">savings</p>
-            </div>
+  return (
+    <SubFooterContainer>
+      <div className="SubFooterDiv">
+        <div className="shareDiv">
+          <p className="share">
+            Share <br /> Crypto via <br /> juat a link<span className="dot">.</span>
+          </p>
+          <div className="perfect">
+            <p>perfect for</p>
+            <p className="gifts">gifts</p>
+            <p className="gifts">payments</p>
+            <p className="gifts">savings</p>
           </div>
-          <div className="shareimg">
-            <img src={ShareImage}  alt="share" srcset="" />
-          </div>
-          </div>
-      </SubFooterContainer>
-    );
-  };
-  
-  export default SubFooter;
-  
+        </div>
+        <div className="shareimg">
+          <img src={ShareImage} alt="share" srcset="" />
+        </div>
+      </div>
+    </SubFooterContainer>
+  );
+};
+
+export default SubFooter;
