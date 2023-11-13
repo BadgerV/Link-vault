15f4d909d1576141fe3b892c157c9a24790d0044
davonjagah <davonkaze1@gmail.com>
2023-11-14 00:39:42 +0100

    Feat: Added Banks endpoint and Resolve Bank Account

--- remitflex/backend/controllers/payment.controller.ts
+++ remitflex/backend/controllers/payment.controller.ts
@@ -3,11 +3,7 @@ import { Response, Request } from "express";
 import Flutterwave from "flutterwave-node-v3";
 
 import { defaultConfig } from "../config/config";
-<<<<<<< HEAD
 import { getVault } from "link-vault";
-=======
-import { getWallet } from "link-vault";
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
 import algosdk from "algosdk";
 
 const algodToken = "";
@@ -18,16 +14,11 @@ const algodClient = new algosdk.Algodv2(algodToken, algodServer, port);
 const url = "https://min-api.cryptocompare.com/data/price?fsym=USDC&tsyms=NGN";
 const headers = new Headers({
   "Content-Type": "application/json",
-<<<<<<< HEAD
   "Authorization": `Apikey ${defaultConfig.CC_API_KEY}` // Add any other headers as needed
-=======
-  Authorization: `Apikey ${defaultConfig.CC_API_KEY}` // Add any other headers as needed
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
 });
 
 const flw = new Flutterwave(defaultConfig.FLW_PUBLIC_KEY, defaultConfig.FLW_SECRET_KEY);
 
-<<<<<<< HEAD
 const USDCNGNRate=async ()=>{
   // Fetch USDC rate
   const result= await fetch(url, {
@@ -39,27 +30,11 @@ const USDCNGNRate=async ()=>{
 
   return data.NGN;
 
-=======
-const USDCNGNRate = async () => {
-  // Fetch USDC rate
-  const result = await fetch(url, {
-    method: "GET",
-    headers: headers
-  });
-
-  const data = await result.json();
-
-  return data.NGN;
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
 };
 
 // Powered By Crypto Compare
 export const getRate = async (req: Request, res: Response) => {
-<<<<<<< HEAD
   const currentRate=await USDCNGNRate();
-=======
-  const currentRate = await USDCNGNRate();
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
 
   //response
   res.status(200).json({
@@ -69,7 +44,6 @@ export const getRate = async (req: Request, res: Response) => {
 
 // Initialize a Bill payment transaction.
 export const initializeBillPayment = async (req: Request, res: Response) => {
-<<<<<<< HEAD
   const linkvaulturl=req.body.linkvaulturl;
   const amount=req.body.amount;
   const rate=await USDCNGNRate();
@@ -78,26 +52,12 @@ export const initializeBillPayment = async (req: Request, res: Response) => {
   try {
     const params = await algodClient.getTransactionParams().do();
     const vault:any =await getVault(linkvaulturl);
-=======
-  const linkvaulturl = req.body.linkvaulturl;
-  const amount = req.body.amount;
-  const rate = await USDCNGNRate();
-  const amountUSD = amount / rate;
-
-  try {
-    const params = await algodClient.getTransactionParams().do();
-    const vault: any = await getWallet(linkvaulturl);
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
 
     const txnParams = {
       from: vault.address,
       to: defaultConfig.MERCHANT_ADDRESS,
       assetIndex: 31566704, // USDC ASSET ID
-<<<<<<< HEAD
       amount: amountUSD * (10 ** 6), // 1 for NFT
-=======
-      amount: amountUSD * 10 ** 6, // 1 for NFT
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
       fee: 1000, // Transaction fee (microAlgos)
       firstRound: params.firstRound,
       lastRound: params.lastRound,
@@ -111,20 +71,13 @@ export const initializeBillPayment = async (req: Request, res: Response) => {
     vaultSK.set(vault.publicKey, vault.privateKey.length);
 
     const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject(txnParams);
-<<<<<<< HEAD
     const signedTxn:any = algosdk.signTransaction(txn, vaultSK);
           
     const { txId } =await algodClient.sendRawTransaction(signedTxn).do();
-=======
-    const signedTxn: any = algosdk.signTransaction(txn, vaultSK);
-
-    const { txId } = await algodClient.sendRawTransaction(signedTxn).do();
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
     const result = await algosdk.waitForConfirmation(algodClient, txId, 4);
     console.log(result);
     console.log(`Transaction Information: ${result.txn}`);
 
-<<<<<<< HEAD
     req.body.reference=txId;
     const payload=req.body;
 
@@ -189,28 +142,6 @@ export const resolveAccount = async (req: Request, res: Response) => {
     res.status(500).json({
       error:{
         message:"Couldnt Resolve User"
-=======
-    req.body.reference = txId;
-    const payload = req.body;
-
-    try {
-      const response = await flw.Bills.create_bill(payload);
-      res.status(200).json({
-        message: "Transaction Initialized.",
-        data: response
-      });
-    } catch (error) {
-      res.status(500).json({
-        error: {
-          message: "Couldn't initialize Bill Payment, Please Contact Support"
-        }
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: {
-        message: "Couldn't initialize transaction from Linkvault."
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
       }
     });
   }
@@ -218,7 +149,6 @@ export const resolveAccount = async (req: Request, res: Response) => {
 
 // Initialize a Bill payment transaction.
 export const initializePayment = async (req: Request, res: Response) => {
-<<<<<<< HEAD
   const linkvaulturl=req.body.linkvaulturl;
   const amount=req.body.amount;
   const rate=await USDCNGNRate();
@@ -227,26 +157,12 @@ export const initializePayment = async (req: Request, res: Response) => {
   try {
     const params = await algodClient.getTransactionParams().do();
     const vault:any =await getVault(linkvaulturl);
-=======
-  const linkvaulturl = req.body.linkvaulturl;
-  const amount = req.body.amount;
-  const rate = await USDCNGNRate();
-  const amountUSD = amount / rate;
-
-  try {
-    const params = await algodClient.getTransactionParams().do();
-    const vault: any = await getWallet(linkvaulturl);
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
 
     const txnParams = {
       from: vault.address,
       to: defaultConfig.MERCHANT_ADDRESS,
       assetIndex: 31566704, // USDC ASSET ID
-<<<<<<< HEAD
       amount: amountUSD * (10 ** 6), // 1 for NFT
-=======
-      amount: amountUSD * 10 ** 6, // 1 for NFT
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
       fee: 1000, // Transaction fee (microAlgos)
       firstRound: params.firstRound,
       lastRound: params.lastRound,
@@ -260,20 +176,13 @@ export const initializePayment = async (req: Request, res: Response) => {
     vaultSK.set(vault.publicKey, vault.privateKey.length);
 
     const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject(txnParams);
-<<<<<<< HEAD
     const signedTxn:any = algosdk.signTransaction(txn, vaultSK);
           
     const { txId } =await algodClient.sendRawTransaction(signedTxn).do();
-=======
-    const signedTxn: any = algosdk.signTransaction(txn, vaultSK);
-
-    const { txId } = await algodClient.sendRawTransaction(signedTxn).do();
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
     const result = await algosdk.waitForConfirmation(algodClient, txId, 4);
     console.log(result);
     console.log(`Transaction Information: ${result.txn}`);
 
-<<<<<<< HEAD
     req.body.reference=txId;
     req.body.callback_url= "https://webhook.site/b3e505b0-fe02-430e-a538-22bbbce8ce0d";
 
@@ -317,30 +226,3 @@ export const initializePayment = async (req: Request, res: Response) => {
 
 
 
-=======
-    req.body.reference = txId;
-    req.body.callback_url = "https://webhook.site/b3e505b0-fe02-430e-a538-22bbbce8ce0d";
-
-    const payload = req.body;
-    try {
-      const response = await flw.Transfer.initiate(payload);
-      res.status(200).json({
-        message: "Transaction Initialized.",
-        data: response
-      });
-    } catch (error) {
-      res.status(500).json({
-        error: {
-          message: "Couldn't initialize Remit Payment, Please Contact Support"
-        }
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: {
-        message: "Couldn't initialize transaction from Linkvault."
-      }
-    });
-  }
-};
->>>>>>> a13d179c441fab8b1ecb7e06056aabb4854a5472
